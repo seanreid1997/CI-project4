0aa992bf86c395f3630febdc58b115055a220aba
seanreid1997 <reid.sean14@yahoo.com>
2023-01-02 23:46:53 +0000

    Install psycopg2 and dj_database_url

--- mynet/settings.py
+++ mynet/settings.py
@@ -33,12 +33,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudinary',
+    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'profiles'
+
 ]
 
 MIDDLEWARE = [
