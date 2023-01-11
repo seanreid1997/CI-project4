b34fec7f7cb904873cd75d7e519c1007f8fefbc9
seanreid1997 <reid.sean14@yahoo.com>
2023-01-11 22:15:01 +0000

    Create Profile model

--- profiles/models.py
+++ profiles/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
+STATUS = ((0, "Draft"), (1, "Posted"))
 
-STATUS = ((0, "Draft"), (1, "Published"))
+
+class Profile(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profile_image = CloudinaryField('image', default='https://t3.ftcdn.net/jpg/03/46/83/96/360_\
+    F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg')
+
+    def __str__(self):
+        return self.user.username
 
 
 class Post(models.Model):
