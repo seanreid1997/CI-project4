35ee60e629d57f47cda592f6f7dc8aeee3aefd4d
seanreid1997 <reid.sean14@yahoo.com>
2023-01-11 23:49:09 +0000

    Style footer and navbar

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('upload/', views.add_post, name='add_post'),
+    path('add_post/', views.add_post, name='add_post'),
 ]
 
